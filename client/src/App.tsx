340ac162ddac61b8d13eb894418c402beb87c50c
Timothy Lee <timothylhy@hotmail.com>
2023-08-29 00:55:01 +0800

    updated login and register with jwt and bcrypt

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,48 +1,15 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Register from './auth/Register';
+import Login from './auth/Login';
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  function handleCreateUser(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    fetch('http://localhost:8000/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ 
-        username, 
-        password,
-      }),
-    });
-  }
-
   return (
     <div className="App">
-      <h2>Login</h2>
-      <form onSubmit={handleCreateUser}>
-        <label htmlFor="login-username">Username:</label>
-        <input id="login-username"
-          type="text"
-          value={username}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setUsername(e.target.value)
-          }}
-        />
-        <label htmlFor="login-password">Password:</label>
-        <input id="login-password"
-          type="text"
-          value={password}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setPassword(e.target.value)
-          }}
-        />
-        <button>Login</button>
-      </form>
+      <Register />
+      <Login />
     </div>
   );
-};
+}
 
 export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,8 @@ import mongoose from 'mongoose';
 import cookieParser from "cookie-parser";
 import UserModel from './models/User';
 import cors from 'cors';
+import bcrypt from "bcrypt";
+import { generateToken, verifyToken } from './utils/jwtHelper';
 
 const PORT = 8000;
 
@@ -14,14 +16,50 @@ app.use(cors({
     origin: "*",
 }));
 app.use(express.json()); // allow for json post requests
+app.use(cookieParser()); // allow for cookie parsing
 
-app.post("/login", async (req: Request , res: Response) => {
-    const newUser = new UserModel({
-        username: req.body.username,
-        password: req.body.password,
-    });
-    const createdUser = await newUser.save();
-    res.json(createdUser);
+// Registration endpoint
+app.post("/register", async (req: Request, res: Response) => {
+    try {
+        // Check if user already exists
+        const existingUser = await UserModel.findOne({ username: req.body.username });
+        if (existingUser) {
+            return res.status(400).send("Username already exists.");
+        }
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        const newUser = new UserModel({
+            username: req.body.username,
+            password: hashedPassword,
+        });
+
+        const createdUser = await newUser.save();
+        res.status(201).json({ message: "User created successfully." });
+    } catch (error) {
+        res.status(500).send("Error registering user.");
+    }
+});
+
+// Login endpoint
+app.post("/login", async (req: Request, res: Response) => {
+    try {
+        const user = await UserModel.findOne({ username: req.body.username });
+        if (!user) {
+            return res.status(400).send("Invalid username or password.");
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) {
+            return res.status(400).send("Invalid username or password.");
+        }
+
+        const token = generateToken(user._id.toString());
+        res.cookie("token", token, { httpOnly: true }).send("Logged in successfully.");
+    } catch (error) {
+        res.status(500).send("Error logging in.");
+    }
 });
 
 mongoose.connect(process.env.MONGO_URL!).then(() => {
