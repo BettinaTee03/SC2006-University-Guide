f92470b85c33307267c1fd7397bef36b46809681
Timothy Lee <timothylhy@hotmail.com>
2023-10-12 14:52:22 +0800

    added jsdocs for backend

--- server/src/config/openAI.js
+++ server/src/config/openAI.js
@@ -1,11 +1,24 @@
-import { config } from 'dotenv';
+import { config } from "dotenv";
 import { OpenAI } from "langchain/llms/openai";
+
 config();
 
-const llm = new OpenAI({
-    openAIApiKey: process.env.OPENAI_API_KEY,
-    modelName: "gpt-3.5-turbo",
-    temperature: 0.7,
- });
+/**
+ * An instance of the OpenAI library.
+ * @typedef {Object} OpenAI
+ * @property {string} openAIApiKey - The API key for OpenAI.
+ * @property {string} modelName - The name of the model to use.
+ * @property {number} temperature - Determines the randomness of the model's output.
+ */
 
- export default llm;
+/**
+ * Creates a new instance of OpenAI with the provided configuration.
+ * @type {OpenAI} llm
+ */
+const llm = new OpenAI({
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  modelName: "gpt-3.5-turbo",
+  temperature: 0.7,
+});
+
+export default llm;
--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -1,22 +1,40 @@
-import passport from 'passport';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import { Strategy as LocalStrategy } from 'passport-local';
-import UserModel from '../models/userModel.js';
-import { config } from 'dotenv';
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as LocalStrategy } from "passport-local";
+import UserModel from "../models/userModel.js";
+import { config } from "dotenv";
+
 config();
 
-passport.use(new GoogleStrategy({
-	clientID: process.env.CLIENT_ID,
-	clientSecret: process.env.CLIENT_SECRET,
-	callbackURL: "http://localhost:8000/auth/google/callback",
-	},
-	function(accessToken, refreshToken, profile, cb) {
-		UserModel.findOrCreate({ googleId: profile.id }, function (err, user) {
-			return cb(err, user);
-		});
-	}
-));
+/**
+ * Initializes Passport.js with Google OAuth 2.0 authentication strategy.
+ * @function
+ * @param {Object} options - Passport.js authentication options.
+ * @param {string} options.clientID - The Google OAuth client ID.
+ * @param {string} options.clientSecret - The Google OAuth client secret.
+ * @param {string} options.callbackURL - The callback URL for Google OAuth.
+ * @param {function} verifyCallback - The callback function to verify and create the user.
+ */
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:8000/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      UserModel.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
 
+/**
+ * Initializes Passport.js with Local strategy for username and password authentication.
+ * @function
+ * @param {function} authenticateCallback - The callback function for authenticating a user.
+ */
 passport.use(new LocalStrategy(UserModel.authenticate()));
 
 export default passport;
