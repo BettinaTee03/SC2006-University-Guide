23f65e26b91975fab7f5395d42906357eee00b86
Timothy Lee <timothylhy@hotmail.com>
2023-10-18 16:38:56 +0800

    testing on heroku

--- server/src/config/session.js
+++ server/src/config/session.js
@@ -13,7 +13,7 @@ const sessionConfig = session({
     maxAge: 3600000,
     secure: process.env.NODE_ENV === "production", // Set secure flag only in production
     httpOnly: true, // Makes the cookie inaccessible from JavaScript on the frontend
-    sameSite: "lax", // For CSRF protection. Optional but recommended
+    sameSite: "none",
   },
   resave: false,
   saveUninitialized: false,
