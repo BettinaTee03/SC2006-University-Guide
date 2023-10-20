931465d1e58e60a4fa8ba7c60a239fc7b4dfd064
Timothy Lee <timothylhy@hotmail.com>
2023-10-20 17:21:07 +0800

    Update session.js back to 1 hour session

--- server/src/config/session.js
+++ server/src/config/session.js
@@ -10,7 +10,7 @@ config();
 const sessionConfig = session({
   secret: process.env.SESSION_SECRET,
   cookie: {
-    maxAge: 60000,
+    maxAge: 3600000,
     secure: process.env.NODE_ENV === "production", // Set secure flag only in production
     httpOnly: true, // Makes the cookie inaccessible from JavaScript on the frontend
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
